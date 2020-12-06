0bfbef6a622b69e09af32f03935c9a8f378ce54d
Alex <alexsaadfalcon@yahoo.com>
2020-12-06 00:31:38 -0500

    Redirected fragments to proper path

--- run.py
+++ run.py
@@ -154,7 +154,7 @@ def vase():
 def vase_outline():
     return send_file('vase_outline.jpg', mimetype='image/jpeg')
 
-frags = glob.glob(app.root_path + '/fragments/*.jpg')
+frags = glob.glob(app.root_path + '/chopped/*.jpg')
 split_id = lambda f: int(os.path.splitext(os.path.split(f)[-1])[0])
 id_dict = {split_id(f): f for f in frags}
 ind_dict = {n: f for n, f in enumerate(frags)}
